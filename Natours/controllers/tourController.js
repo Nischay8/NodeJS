689c5d3cce0a098c7cc9df6ccb77090c3fb3c280
nischay <nischey.me008@gmail.com>
2024-09-13 00:16:00 +0530

    Added on 13 September 2024

--- Natours/controllers/tourController.js
+++ Natours/controllers/tourController.js
@@ -1,92 +1,71 @@
-const fs = require('fs');
+/* eslint-disable node/no-unsupported-features/es-syntax */
+const Tour = require('../models/TourModel');
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
 
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`);
-
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
+    res.status(201).json({
+      status: 'Success',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'Fail',
+      message: error
     });
   }
-  next();
 };
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price'
+exports.getAllTours = async (req, res) => {
+  try {
+    const allTours = await Tour.find({});
+    res.status(200).json({
+      status: 'Success',
+      data: allTours
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'Fail',
+      message: error
     });
   }
-  next();
 };
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
 
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours
-    }
-  });
+    res.status(200).json({
+      status: 'Success',
+      data: tour
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'Fail',
+      message: error
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
+exports.updateTour = async (req, res) => {
+  try {
+    const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    });
 
-  const tour = tours.find(el => el.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    }
-  });
-};
-
-exports.createTour = (req, res) => {
-  // console.log(req.body);
-
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    err => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour
-        }
-      });
-    }
-  );
-};
-
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>'
-    }
-  });
-};
-
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+    res.status(200).json({
+      status: 'Success',
+      data: {
+        tour: updatedTour
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'Fail',
+      message: error
+    });
+  }
 };
--- Natours/server.js
+++ Natours/server.js
@@ -1,7 +1,21 @@
 const dotenv = require('dotenv');
+
+const mongoose = require('mongoose');
+
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const db = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log('Db is Connected');
+  })
+  .catch(err => {
+    console.log('Erro while Making Connection to Database', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
